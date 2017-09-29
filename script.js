76d8a2b4725cc502e75665a5fda4cfee20ad621b
Renan Roberto <renanroberto1@gmail.com>
2017-09-28 23:01:06 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const app = new Vue({
+  el: '#app',
+
+  data: {
+    newTask: '',
+    tasks: [
+      { text: 'Example task', done: false }
+    ]
+  },
+
+  methods: {
+    add () {
+      if (this.newTask !== '') {
+        this.tasks.push({ text: this.newTask, done: false })
+      }
+      this.newTask = ''
+    },
+
+    remove (task) {
+      let index = this.tasks.indexOf(task)
+      if (index > -1) this.tasks.splice(index, 1)
+    }
+  }
+})
