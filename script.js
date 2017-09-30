f541a23187f7420818743b773735480ac11483da
Renan Roberto <renanroberto1@gmail.com>
2017-09-29 21:33:02 -0300

    build

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const db = new PouchDB('tasks')
+
 const app = new Vue({
   el: '#app',
 
@@ -5,7 +7,8 @@ const app = new Vue({
     newTask: '',
     tasks: [
       { text: 'Example task', done: false }
-    ]
+    ],
+    toSave: { _id: 'new Date().toISOString()', data: this.tasks}
   },
 
   methods: {
@@ -19,6 +22,26 @@ const app = new Vue({
     remove (task) {
       let index = this.tasks.indexOf(task)
       if (index > -1) this.tasks.splice(index, 1)
+    },
+
+    // save each item
+    save () {
+      db.put(this.toSave, (err, res) => {
+        if (!err) console.log("Save succeful!")
+        else console.log('ERROR: ' + err)
+      })
+    },
+
+    // print loaded docs
+    load () {
+      db.allDocs({include_docs: true, descending: true}, function(err, doc) {
+        if (!err) console.log(doc.rows)
+        else console.log('ERROR: ' + err)
+      })
     }
+  },
+
+  beforeMount: function () {
+    this.load()
   }
 })
